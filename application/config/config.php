d3328830499864ebb5369f848ff33e1de84f7f0b
shyntaayu <shynta.add@gmail.com>
2019-04-09 12:58:28 +0700

    + ilangi folder

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://mimulwebapp.azurewebsites.net/user-SQLServer/';
+$config['base_url'] = 'https://mimulwebapp.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
