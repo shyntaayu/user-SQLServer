c56d0fc7bf6fcb6c4c60685d5b507ddb4f283bd2
shyntaayu <shynta.add@gmail.com>
2019-04-09 12:54:42 +0700

    + ganti base_url + dafault controller + .htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/user-SQLServer/';
+$config['base_url'] = 'http://mimulwebapp.azurewebsites.net/user-SQLServer/';
 
 /*
 |--------------------------------------------------------------------------
